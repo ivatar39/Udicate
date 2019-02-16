f3cdae73d672b83265246ef7115f482f3c1790e4
ivatar39 <ivlupsecond@gmail.com>
2019-02-16 22:46:09 +0200

    Small fix

---
+++ app/src/main/java/com/ivlup/udicate/activity/ChooseSubjectsActivity.java
@@ -0,0 +1,15 @@
+package com.ivlup.udicate.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ivlup.udicate.R;
+
+public class ChooseSubjectsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+
+}
