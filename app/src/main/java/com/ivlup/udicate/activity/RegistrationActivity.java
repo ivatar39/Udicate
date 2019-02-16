177ca692af3f1b6aa4479cab240636f929621e40
a-genio <47211857+a-genio@users.noreply.github.com>
2019-02-16 20:32:00 +0200

    Almost finished registration

--- app/src/main/java/com/ivlup/udicate/activity/RegistrationActivity.java
+++ app/src/main/java/com/ivlup/udicate/activity/RegistrationActivity.java
@@ -11,43 +11,31 @@ import android.view.View;
 import android.widget.Button;
 
 import com.ivlup.udicate.R;
-import com.ivlup.udicate.fragment.RegStageFirstFragment;
-import com.ivlup.udicate.fragment.RegStageSecondFragment;
 
 public class RegistrationActivity extends AppCompatActivity {
     Fragment registration, choose;
     FragmentTransaction fTrans;
-    Button submit;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
-        choose = new RegStageFirstFragment();
-        fragmentTransaction.replace(R.id.frame, choose);
-        fragmentTransaction.commit();
+        register = (Button) (findViewById(R.id.buttonRegister));
 
-        submit = (Button) (findViewById(R.id.bt_submit));
-        submit.setOnClickListener(new View.OnClickListener() {
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (RegStageFirstFragment.getSelected() == -1) {
-                    registration = new RegStageSecondFragment();
-                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction1.replace(R.id.frame, registration);
-                    fragmentTransaction1.commit();
-                }
-
-                if (RegStageSecondFragment.registrationCompleted()) {
-                    Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+                createMainIntent();
             }
         });
 
+    }
+
+    private void createMainIntent() {
+        Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
