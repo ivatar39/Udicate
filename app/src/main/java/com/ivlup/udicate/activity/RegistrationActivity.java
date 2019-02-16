789355f57e4df3b9603d206f4e9caaa407419ec0
Artem <artemon1002@gmail.com>
2019-02-16 22:33:22 +0200

    Merge branch 'master' of C:\projects\Udicate with conflicts.

--- app/src/main/java/com/ivlup/udicate/activity/RegistrationActivity.java
+++ app/src/main/java/com/ivlup/udicate/activity/RegistrationActivity.java
@@ -2,20 +2,18 @@ package com.ivlup.udicate.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-
-import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.ivlup.udicate.R;
 
 public class RegistrationActivity extends AppCompatActivity {
+    Fragment registration, choose;
+    FragmentTransaction fTrans;
     Button register;
-    TextInputEditText mLogin, mName, mEmail, mPassword;
-    String login, name, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +22,10 @@ public class RegistrationActivity extends AppCompatActivity {
 
 
         register = (Button) (findViewById(R.id.buttonRegister));
-        mLogin = (TextInputEditText) (findViewById(R.id.editTextUserLogin));
-        mName = (TextInputEditText) (findViewById(R.id.editTextUserName));
-        mEmail = (TextInputEditText) (findViewById(R.id.editTextEmail));
-        mPassword = (TextInputEditText) (findViewById(R.id.editTextPassword));
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gatherInfo();
                 createMainIntent();
             }
         });
@@ -42,17 +35,5 @@ public class RegistrationActivity extends AppCompatActivity {
     private void createMainIntent() {
         Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
         startActivity(intent);
-    }
-
-    private void gatherInfo() {
-        if (mEmail.getText().length() > 3 && mPassword.getText().length() > 3) {
-            email = String.valueOf(mEmail.getText());
-            password = String.valueOf(mPassword.getText());
-            login = String.valueOf(mLogin.getText());
-            name = String.valueOf(mName.getText());
-        }
-        else {
-            Toast.makeText(this, "Не все поля заполнены!" , Toast.LENGTH_SHORT).show();
-        }
     }
 }
--- app/src/main/java/com/ivlup/udicate/backend/repository/FirebaseRepository.java
+++ app/src/main/java/com/ivlup/udicate/backend/repository/FirebaseRepository.java
@@ -1,5 +1,65 @@
 package com.ivlup.udicate.backend.repository;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+
 public abstract class FirebaseRepository {
-    
+    private static FirebaseAuth mAuth;
+    @SuppressLint("StaticFieldLeak")
+    private static GoogleSignInClient mGoogleSignInClient;
+
+    public static void setmGoogleSignInClient(GoogleSignInClient mGoogleSignInClient) {
+        FirebaseRepository.mGoogleSignInClient = mGoogleSignInClient;
+    }
+
+    boolean checkUser(){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.i("Test_log", String.valueOf(currentUser));
+        return true;
+    }
+
+    void emailPasswordLogin(FirebaseAuth fa, String email, String password){
+        setmAuth(fa);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("MyTag", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.i("Auth", "Всё окич )");
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.i("Auth", "Догин или пароль введены не верно");
+                        }
+                    }
+                });
+    }
+    void configueGoogleSign(Context context){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        setmGoogleSignInClient(GoogleSignIn.getClient(context, gso));
+    }
+    void googleLogin(Context context){
+
+    }
+
+    public void setmAuth(FirebaseAuth mAuth) {
+        this.mAuth = mAuth;
+    }
 }
